f90cbba64ff61e37551228fec45fd507218aff1a
AL-Taco <ineed.antonioluis.taco@gmail.com>
2025-07-21 04:20:35 +0800

    Update portfolio

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// script.js
 document.querySelector('.click-to-open').addEventListener('click', () => {
-  alert('Launching Project: @OC-UDI!');
-});
-
-// Smooth scroll for internal links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
+  alert('Launching Project 1: DOC-UDI!');
 });
